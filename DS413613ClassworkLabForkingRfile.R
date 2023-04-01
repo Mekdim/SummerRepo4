82ae9acb7abca11db0f04955979d04dd0a60882b
Mekdim <mekdimashebo@gmail.com>
2023-03-31 20:35:16 -0400

    Update DS413613ClassworkLabForkingRfile.R

--- DS413613ClassworkLabForkingRfile.R
+++ DS413613ClassworkLabForkingRfile.R
@@ -2,11 +2,12 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 mpg
-# My name is John Doe
-# My favorite color is blue
+# My name is Mekdim Ashebo
+# The datasource is the mpg datatable
 # This is my coding that generates Boxplots for transmission types
 ggplot(data = mpg) +
   geom_boxplot(mapping = aes(x = drv, y = displ, fill = drv)) +
+  coord_flip() +
   ggtitle("Boxplots (mpg data : transmission types vs displacement)") +
   xlab("transmission types") +
   ylab("displacement") 
